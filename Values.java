d26079dd88162c61f3912fb5d593fe342ed4212f
sowmiyalaxmisubbaiyaa <sowmiyalaxmisubbaiyaa@gmail.com>
2016-09-02 11:01:07 +0530

    Update and rename README.md to Values.java

---
+++ Values.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class V alues
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int input=sc.nextInt();
+if(input>0)
+{
+System.out.println("positive");
+}
+else if(input==0)
+{
+System.out.println("zero");
+}
+else if(input<0)
+{
+System.out.println("negative");
+}
+}
+}
+
+
