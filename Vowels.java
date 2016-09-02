603c4d6dffa23da2d6510fcd9a0bbf1e4137992d
sowmiyalaxmisubbaiyaa <sowmiyalaxmisubbaiyaa@gmail.com>
2016-09-02 11:24:33 +0530

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class Vowels
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+char s=sc.next.charAt(0);
+if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+{
+System.out.println("vowels");
+}
+else
+System.out.println("consonets");
+}
+}
